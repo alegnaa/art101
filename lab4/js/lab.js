daf5b572118a71b677d75912ecbd820837903898
alegnaa <angela.amb040728@gmail.com>
2023-10-22 22:45:45 -0700

    lab4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,6 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Mancala Game
+// Author: Adrian Contreras and Angela Ambriz
+// Date: October 2024
 
 // Constants
 
@@ -11,6 +11,17 @@ function myFunction(param1, param2) {
   // some code here
   // return results;
 }
+// Print Board
+  // 6 circles on each side with a long oval on either side of the board
+  // 4 stones in each circle
+// Two users
+// Player 1 chooses a circle on their side to take stones from
+  // Pick up the stones in the space chosen
+// Starting with the next space, drop one stone in each space going counterclockwise (including the Store) until all of the stones are played out
+  // A player can go again if the stone ends in their store
+// Once player 1 can not move anymore player 2 repeats step b
+// Step b is continued until a player has no more stones in their six circles. 
+// Test for win or draw condition
 
 function main() {
   console.log("Main function started.");
